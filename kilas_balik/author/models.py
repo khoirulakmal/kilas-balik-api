fd50923d324a0493184007dfb98305951f84a1b6
Khoirul Akmal <khoirulakmal.w@gmail.com>
2023-04-28 19:43:35 +0700

    Create model for authors

--- kilas_balik/author/models.py
+++ kilas_balik/author/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-
+# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    descriptions = models.TextField()
+    
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField()
